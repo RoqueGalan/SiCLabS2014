adf40a47e05c5325b1414285e358fd74813b06ac
Roque Galán <Roque Galn@Notebook_81>
2014-10-20 01:04:22 -0500

    DocumentoEspacio terminado{ 	validaciones: php y jquery 	select 	insert 	update 	delete: pregunta jquery }

--- modelo/ImagenNoticia.php
+++ modelo/ImagenNoticia.php
@@ -63,9 +63,9 @@ class ImagenNoticia extends Modelo {
 //    private $_Ruta;
 //    private $_NoticiaId;
     // ------------------- metodos de la bd ----------------------
-    public function lista() {
+    public function lista($where = '') {
         $lista = array();
-        $tempLista = $this->_db->select('SELECT * FROM ImagenNoticia ');
+        $tempLista = $this->_db->select('SELECT * FROM ImagenNoticia ' . $where);
 
         //crear una lista de objetos, para su facil extracion en las vistas
         foreach ($tempLista as $temp) {
